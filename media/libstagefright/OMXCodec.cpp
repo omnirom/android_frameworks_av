51f32f092fe904a10aa8d5e5e9a3a339c9b1a8c6
Jayasena Sangaraboina <jsangara@codeaurora.org>
2012-01-16 17:40:40 -0800

    libstagefright: Handle error on allocate buffer failure
    Set state to error on allocate buffer failure at start(init)
    of decoding.
    
    Change-Id: Ice54f2d3f9cb087dd1f8f14913aacb632c4a8f9a
    CRs-Fixed: 332373

--- media/libstagefright/OMXCodec.cpp
+++ media/libstagefright/OMXCodec.cpp
@@ -1888,6 +1888,8 @@ status_t OMXCodec::init() {
 
     err = allocateBuffers();
     if (err != (status_t)OK) {
+        CODEC_LOGE("Allocate Buffer failed - error = %d", err);
+        setState(ERROR);
         return err;
     }
 
