98b9c58ac9625bc3526de61330d6f0f48dab92af
Haynes Mathew George <hgeorge@codeaurora.org>
2012-08-29 22:58:51 +0300

    libstagefright: support for disabling buffer metadata
    - Metada mode video recording is enabled by default.
    - use setprop debug.camcorder.disablemeta 1 to disable metadata mode recording.
    
    Change-Id: I422c49c0ace0c3a3e1f4459c7e4bf29e70af763a

--- media/libmediaplayerservice/StagefrightRecorder.cpp
+++ media/libmediaplayerservice/StagefrightRecorder.cpp
@@ -58,6 +58,9 @@
 #endif
 
 #include "ARTPWriter.h"
+#ifdef QCOM_HARDWARE
+#include <cutils/properties.h>
+#endif
 
 namespace android {
 
@@ -1483,10 +1486,18 @@ status_t StagefrightRecorder::setupCameraSource(
                 mTimeBetweenTimeLapseFrameCaptureUs);
         *cameraSource = mCameraSourceTimeLapse;
     } else {
+        bool useMeta = true;
+#ifdef QCOM_HARDWARE
+        char value[PROPERTY_VALUE_MAX];
+        if (property_get("debug.camcorder.disablemeta", value, NULL) &&
+            atoi(value)) {
+            useMeta = false;
+        }
+#endif
         *cameraSource = CameraSource::CreateFromCamera(
                 mCamera, mCameraProxy, mCameraId, mClientName, mClientUid,
                 videoSize, mFrameRate,
-                mPreviewSurface, true /*storeMetaDataInVideoBuffers*/);
+                mPreviewSurface, useMeta /*storeMetaDataInVideoBuffers*/);
     }
     mCamera.clear();
     mCameraProxy.clear();
