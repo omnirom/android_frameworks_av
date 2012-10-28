4e56181ef90103a28f63073f882f4e640f23ef98
Alexey Shavykin <ganster0x29@gmail.com>
2012-10-28 22:46:34 +0200

    legacy OMX: squashed commit for legacy OMX support
    This is a combination of 5 commits.
    
    commit ff5604878d2bf74555adcf938ffbf98d04bea305
    Author: Alexey Shavykin <ganster0x29@gmail.com>
    Date:   Sun Oct 28 22:46:34 2012 +0200
    
        Added support legacy Qualcomm OpenMAX IL libs for MSM7x27
    
        Change-Id: I63e68e63fce955fef52813360032f5e8e90a4b92
    
    commit 1a7ef5d4c8d1eebf1851b3b5b46c629a19a2c304
    Author: WinSuk <goodboy735@gmail.com>
    Date:   Wed Nov 14 23:03:24 2012 -0800
    
        Fix the encoder
    
        Change-Id: I929c769b24d0b891487faf4f0d3c9fa66e04eeef
    
    commit 27cf702ac81f27cae206ef182420c1cbcdadb709
    Author: Ganster41 <ganster0x29@gmail.com>
    Date:   Mon Nov 19 15:35:36 2012 +0200
    
        Correct gralloc usage flags for renderer
    
    commit 3feab8906fcb4464d5f46bc13dcae9da36e41040
    Author: Conn O'Griofa <connogriofa@gmail.com>
    Date:   Fri Jan 4 00:31:13 2013 -0200
    
        Fix legacy OMX encoders
    
        Ensure that a valid frame_rate is passed to the encoder, otherwise
        encoding will fail.
    
        Change-Id: Iaaa298496755ff405e393120973d1ed782f5d8f4
    
    commit f03627bce1de975ad9b88206874dda851b7d2bf4
    Author: Grigori Goronzy <greg@blackbox>
    Date:   Sat Jan 19 18:26:25 2013 +0100
    
        Fix alignment and copy size for legacy QCOM OMX
    
        Change-Id: I6d13c67601da0f0c35d3dd455401f982406cefe3
    
    Change-Id: Ied42c26c0da0ba0b229328adbfaaba7f60e77483

--- media/libstagefright/colorconversion/Android.mk
+++ media/libstagefright/colorconversion/Android.mk
@@ -9,6 +9,10 @@ LOCAL_C_INCLUDES := \
         $(TOP)/frameworks/native/include/media/openmax \
         $(TOP)/hardware/msm7k
 
+ifeq ($(TARGET_QCOM_DISPLAY_VARIANT),legacy)
+        LOCAL_C_INCLUDES += $(TOP)/hardware/qcom/display-legacy/libgralloc
+endif
+
 LOCAL_MODULE:= libstagefright_color_conversion
 
 include $(BUILD_STATIC_LIBRARY)
