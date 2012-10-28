cf035af927eea29ae01e0534cc6d2182435efb4e
Alexey Shavykin <ganster0x29@gmail.com>
2012-10-28 22:46:34 +0200

    legacy OMX: squashed commit for legacy OMX support
    Enable by setting TARGET_QCOM_LEGACY_OMX := true in BoardConfig.mk.
    
    This is a combination of 7 commits.
    
    commit ff5604878d2bf74555adcf938ffbf98d04bea305
    Author: Alexey Shavykin <ganster0x29@gmail.com>
    Date:   Sun Oct 28 22:46:34 2012 +0200
    
        Added support legacy Qualcomm OpenMAX IL libs for MSM7x27
    
        Change-Id: I63e68e63fce955fef52813360032f5e8e90a4b92
    
    commit 27cf702ac81f27cae206ef182420c1cbcdadb709
    Author: Ganster41 <ganster0x29@gmail.com>
    Date:   Mon Nov 19 15:35:36 2012 +0200
    
        Correct gralloc usage flags for renderer
    
    commit 3feab8906fcb4464d5f46bc13dcae9da36e41040
    Author: Conn O'Griofa <connogriofa@gmail.com>
    Date:   Fri Jan 4 00:31:13 2013 -0200
    
        Fix legacy OMX encoders
    
        Ensure that a valid frame_rate is passed to the encoder, otherwise
        encoding will fail.
    
        Change-Id: Iaaa298496755ff405e393120973d1ed782f5d8f4
    
    commit f03627bce1de975ad9b88206874dda851b7d2bf4
    Author: Grigori Goronzy <greg@blackbox>
    Date:   Sat Jan 19 18:26:25 2013 +0100
    
        Fix alignment and copy size for legacy QCOM OMX
    
        Change-Id: I6d13c67601da0f0c35d3dd455401f982406cefe3
    
    commit 3e6134027e9faa6b24b1cb1d205e4de0e76f9ed2
    Author: Ganster41 <ganster0x29@gmail.com>
    Date:   Sat Jan 5 19:33:44 2013 +0200
    
        Fixed Gingerbread mmparser using
    
        Change-Id: Ib931fa6ed4522b5851d42297f929183cf76043fd
    
    commit 25edf9884ebbd3c608edc88d8bd8a7196d58b190
    Author: Ganster41 <ganster0x29@gmail.com>
    Date:   Mon Jan 7 19:56:32 2013 +0200
    
        Try use ExtendedExtractor only if no suitable default extractors available f
    
        Change-Id: I57a9cc39caee7e3fa2b709ab09cca5629bf81f44
    
    commit 9e983100a684493d6c2df7952af4fa04f252c19b
    Author: Konsta <konsta09@gmail.com>
    Date:   Sun Oct 6 20:30:50 2013 +0300
    
        legacy OMX: use local flags
    
        Allows use of legacy mmparser without enabling all QCOM A/V
        enhancements that are not supported with legacy OMX blobs.
    
        Change-Id: Ieccf29ed8036864634ee167c1e21908377516494
    
    Change-Id: Ieccf29ed8036864634ee167c1e21908377516494

--- media/libstagefright/colorconversion/Android.mk
+++ media/libstagefright/colorconversion/Android.mk
@@ -9,6 +9,11 @@ LOCAL_C_INCLUDES := \
         $(TOP)/frameworks/native/include/media/openmax \
         $(TOP)/hardware/msm7k
 
+ifeq ($(TARGET_QCOM_LEGACY_OMX),true)
+        LOCAL_CFLAGS += -DQCOM_LEGACY_OMX
+        LOCAL_C_INCLUDES += $(TOP)/hardware/qcom/display-legacy/libgralloc
+endif
+
 LOCAL_MODULE:= libstagefright_color_conversion
 
 include $(BUILD_STATIC_LIBRARY)
