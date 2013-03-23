2a16885b483521dcc737feba6f946a96d7c64f76
tbalden <illespal@gmail.com>
2013-03-23 23:06:45 +0100

    Camera: QCOM legacy definitions,HTC cam switch,NO_UPDATE_PREVIEW
    Updating Camera parameters, and service with several
    compatibility patches:
    
    1., QCOM legacy CameraParameters and functions:
    Adding these will make ICS qcomm camera blobs work on JB 4.3.
    Use BOARD_USES_QCOM_LEGACY_CAM_PARAMS := true in BoardConfig
    to use this.
    
    Also fixes the problem of legacy qcom
    camera blobs with recent libcameraservice QCOM_HARDWARE changes,
    where the orientation is messed up on first
    preview: the fix is leaving out setPreviewWindow(window) for
    legacy qcomm cam blobs.
    
    2., Also adding NO_UPDATE_PREVIEW flag for legacy purposes.
    
    3., Adding adding the set orientation patch:
    Always set buffers orientation when setting the qcom camera preview window
    Fixes the rotated preview on TouchPad
    https://github.com/CyanogenMod/android_frameworks_base/commit/1a8e41a3c7434737db89b604352575f8b3109e7a
    
    4., camera: Full support for HTC camera switch
    
    5., Adding more parameters for HTC Explorer
    
    Change-Id: I2bfc72b8ef027665356788f1db7f96b31d037dbe

--- services/camera/libcameraservice/api1/CameraClient.cpp
+++ services/camera/libcameraservice/api1/CameraClient.cpp
@@ -254,6 +254,11 @@ void CameraClient::disconnect() {
 
     // Release the held ANativeWindow resources.
     if (mPreviewWindow != 0) {
+#ifdef QCOM_HARDWARE
+#ifndef NO_UPDATE_PREVIEW
+        mHardware->setPreviewWindow(0);
+#endif
+#endif
         disconnectWindow(mPreviewWindow);
         mPreviewWindow = 0;
         mHardware->setPreviewWindow(mPreviewWindow);
@@ -295,6 +300,15 @@ status_t CameraClient::setPreviewWindow(const sp<IBinder>& binder,
             native_window_set_buffers_transform(window.get(), mOrientation);
             result = mHardware->setPreviewWindow(window);
         }
+#ifdef QCOM_HARDWARE
+#ifndef NO_UPDATE_PREVIEW
+    } else {
+        if (window != 0) {
+            native_window_set_buffers_transform(window.get(), mOrientation);
+        }
+        result = mHardware->setPreviewWindow(window);
+#endif
+#endif
     }
 
     if (result == NO_ERROR) {
