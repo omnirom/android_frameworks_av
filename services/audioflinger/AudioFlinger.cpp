9268bcc019d35935418a9e0070ca12c1fe08a9e4
Mingming Yin <mingming@codeaurora.org>
2013-03-29 22:41:28 -0700

    audio: Add support for tunnel mode recording and compress voip
    - Use modulo operator for common block buffer handling. Common block
      size is rounded to power of two. This limitation is avoided with
      modulo operator.
    - Add support for tunnel mode recording
    - Add support for compress voip
    
    Change-Id: I95cdfff729affd784141487521c9f2f714221d11
    
    audio: check audio mode to enable voice path for VoIP apps
    
    - Add check for MODE_IN_COMMUNICATION to enable voice path
    for VoIP applications
    
    Change-Id: Ib603c6321c935d3b1313c2841b09eea932aa13f3
    
    audio: Restrict opening of voip direct output to one instance
    
    Avoid opening of voip direct output track if it is already in use
    by another voip application.
    
    Change-Id: I740bbb9e7153c8871c296f1ec5d8d68997391779
    
    audio: check voip input sample rate to allow voip direct output
    
    It is possible that some voip applications request different sample
    rates for input and output streams. Avoid opening of voip direct
    output if the voip input is already opened with a different sample
    rate.
    
    Change-Id: I5669e2a0880ffed0c10cde21da8994c2b47e1878
    CRs-fixed: 601785
    
    Conflicts:
    	include/media/stagefright/AudioSource.h
    	media/libstagefright/AudioSource.cpp
    	services/audioflinger/AudioFlinger.cpp

--- services/audioflinger/AudioFlinger.cpp
+++ services/audioflinger/AudioFlinger.cpp
@@ -2,6 +2,8 @@
 ** Copyright (c) 2011-2013, The Linux Foundation. All rights reserved.
 ** Not a Contribution.
 ** Copyright 2007, The Android Open Source Project
+** Copyright (c) 2011-2013, The Linux Foundation. All rights reserved.
+** Not a Contribution.
 **
 ** Licensed under the Apache License, Version 2.0 (the "License");
 ** you may not use this file except in compliance with the License.
