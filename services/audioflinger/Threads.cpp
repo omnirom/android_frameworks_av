c22d1f1115458ff3a1f57f10c0542f0cdaf045b3
Pavan Chikkala <pavanc@codeaurora.org>
2013-10-09 19:29:20 +0530

    audio: Fix for race conditions in direct audio track
    -Issue: Mediaserver crash is observed in direct track functions.
    -Rootcause: Between write and flush functions, write and destructor
    effectpool buffers are accessed without any synchronization that
    might lead to operations on null buffers and cause crash.
    -Fix: Use mEffectLock to synchronize access of effectpool buffers.
    
    Change-Id: I5982e31fd6d292a9fe28dfee4cf75ebfe0ade5e6
    CRs-Fixed: 515107
    (cherry picked from commit b14c28f0d3b51ee989640aa46f3addcfabccb6be)
    (cherry picked from commit 782f8512023facb1b655fb636bdcbd19af21b493)
    (cherry picked from commit 1601287d6a06e11d782a821791bf1f5a9dcbd188)

--- services/audioflinger/Threads.cpp
+++ services/audioflinger/Threads.cpp
@@ -5378,8 +5378,10 @@ ssize_t AudioFlinger::DirectAudioTrack::write(const void *buffer, size_t size) {
 
 void AudioFlinger::DirectAudioTrack::flush() {
     if (mFlag & AUDIO_OUTPUT_FLAG_LPA) {
+        mEffectLock.lock();
         mEffectsPool.clear();
         mEffectsPool = mBufPool;
+        mEffectLock.unlock();
     }
     mOutputDesc->stream->flush(mOutputDesc->stream);
 }
@@ -5460,6 +5462,7 @@ void AudioFlinger::DirectAudioTrack::deallocateBufPool() {
 
     //1. Deallocate the local memory
     //2. Remove all the buffers from bufpool
+    mEffectLock.lock();
     while (!mBufPool.empty())  {
         List<BufferInfo>::iterator it = mBufPool.begin();
         BufferInfo &memBuffer = *it;
@@ -5471,6 +5474,8 @@ void AudioFlinger::DirectAudioTrack::deallocateBufPool() {
         ALOGV("Removing from bufpool");
         mBufPool.erase(it);
     }
+    mEffectsPool.clear();
+    mEffectLock.unlock();
 
     free(mEffectsThreadScratchBuffer);
     mEffectsThreadScratchBuffer = NULL;
