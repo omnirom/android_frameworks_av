e5de40a4383cfdfd6c41d9f6705c5e83872572bd
yajnab <yajnab@gmail.com>
2013-10-30 11:11:41 +0530

    make TARGET_QCOM_MEDIA_VARIANT more parametric
    Change-Id: I600f1e64396f36ba9e86aa0b2cc706832d186a0f

--- media/libmediaplayerservice/Android.mk
+++ media/libmediaplayerservice/Android.mk
@@ -54,17 +54,12 @@ LOCAL_C_INCLUDES :=                                                 \
     $(TOP)/external/tremolo/Tremolo                                 \
 
 ifeq ($(BOARD_USES_QCOM_HARDWARE),true)
-    ifeq ($(TARGET_QCOM_MEDIA_VARIANT),caf)
+    ifeq ($(TARGET_QCOM_MEDIA_VARIANT),)
     LOCAL_C_INCLUDES += \
-            $(TOP)/hardware/qcom/media-caf/mm-core/inc
+            $(TOP)/hardware/qcom/media-$(TARGET_QCOM_MEDIA_VARIANT)/mm-core/inc
     else
-        ifeq ($(TARGET_QCOM_MEDIA_VARIANT),legacy)
-            LOCAL_C_INCLUDES += \
-                $(TOP)/hardware/qcom/media-legacy/mm-core/inc
-        else
             LOCAL_C_INCLUDES += \
                 $(TOP)/hardware/qcom/media/mm-core/inc
-        endif
     endif
 endif
 
@@ -73,17 +68,12 @@ LOCAL_MODULE:= libmediaplayerservice
 ifeq ($(TARGET_ENABLE_QC_AV_ENHANCEMENTS),true)
     LOCAL_CFLAGS += -DENABLE_QC_AV_ENHANCEMENTS
     LOCAL_C_INCLUDES += $(TOP)/frameworks/av/include/media
-    ifeq ($(TARGET_QCOM_MEDIA_VARIANT),caf)
-        LOCAL_C_INCLUDES += \
-            $(TOP)/hardware/qcom/media-caf/mm-core/inc
-    else
-        ifeq ($(TARGET_QCOM_MEDIA_VARIANT),legacy)
-            LOCAL_C_INCLUDES += \
-                $(TOP)/hardware/qcom/media-legacy/mm-core/inc
+    ifeq ($(TARGET_QCOM_MEDIA_VARIANT),)
+      LOCAL_C_INCLUDES += \
+           $(TOP)/hardware/qcom/media-$(TARGET_QCOM_MEDIA_VARIANT)/mm-core/inc
         else
-            LOCAL_C_INCLUDES += \
+           LOCAL_C_INCLUDES += \
                 $(TOP)/hardware/qcom/media/mm-core/inc
-        endif
     endif
 endif #TARGET_ENABLE_QC_AV_ENHANCEMENTS
 
--- media/libstagefright/Android.mk
+++ media/libstagefright/Android.mk
@@ -105,17 +105,12 @@ ifeq ($(TARGET_QCOM_AUDIO_VARIANT),caf)
     LOCAL_CFLAGS += -DQCOM_ENHANCED_AUDIO
 endif
 
-ifeq ($(TARGET_QCOM_MEDIA_VARIANT),caf)
+ifeq ($(TARGET_QCOM_MEDIA_VARIANT),)
 LOCAL_C_INCLUDES += \
-        $(TOP)/hardware/qcom/media-caf/mm-core/inc
-else
-    ifeq ($(TARGET_QCOM_MEDIA_VARIANT),legacy)
-        LOCAL_C_INCLUDES += \
-            $(TOP)/hardware/qcom/media-legacy/mm-core/inc
+        $(TOP)/hardware/qcom/media-$(TARGET_QCOM_MEDIA_VARIANT)/mm-core/inc
     else
         LOCAL_C_INCLUDES += \
             $(TOP)/hardware/qcom/media/mm-core/inc
-endif
 endif
 
 LOCAL_SHARED_LIBRARIES := \
@@ -192,17 +187,12 @@ ifeq ($(TARGET_ENABLE_QC_AV_ENHANCEMENTS),true)
     LOCAL_CFLAGS += -DENABLE_QC_AV_ENHANCEMENTS
     LOCAL_SRC_FILES  += ExtendedWriter.cpp
     LOCAL_SRC_FILES  += QCMediaDefs.cpp
-    ifeq ($(TARGET_QCOM_MEDIA_VARIANT),caf)
+    ifeq ($(TARGET_QCOM_MEDIA_VARIANT),)
         LOCAL_C_INCLUDES += \
-            $(TOP)/hardware/qcom/media-caf/mm-core/inc
-    else
-        ifeq ($(TARGET_QCOM_DISPLAY_VARIANT),legacy)
-            LOCAL_C_INCLUDES += \
-                $(TOP)/hardware/qcom/media-legacy/mm-core/inc
+            $(TOP)/hardware/qcom/media-$(TARGET_QCOM_MEDIA_VARIANT)/mm-core/inc
         else
             LOCAL_C_INCLUDES += \
                 $(TOP)/hardware/qcom/media/mm-core/inc
-        endif
     endif
 endif #TARGET_ENABLE_QC_AV_ENHANCEMENTS
 
