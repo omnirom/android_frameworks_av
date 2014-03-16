a8526494d57ed773bfc9f30d881bde1dd2996dce
Pawit Pornkitprasan <p.pawit@gmail.com>
2014-03-16 09:46:00 +0700

    ifdef "stagefright: Enable GraphicBuffer usage to decoder before querying formats"
    Breaks video playback on galaxysmtd
    
    I/OMXCodec(  151): [OMX.SEC.AVC.Decoder] AVC profile = 66 (Baseline), level = 30
    I/OMXCodec(  151): [OMX.SEC.AVC.Decoder] video dimensions are 640 x 352
    W/GraphicBufferAllocator(  148): alloc(640, 352, 19, 00002930, ...) failed -22 (Invalid argument)
    E/        (  148): GraphicBufferAlloc::createGraphicBuffer(w=640, h=352) failed (Invalid argument), handle=0x0
    E/BufferQueue(  148): [SurfaceView] dequeueBuffer: SurfaceComposer::createGraphicBuffer failed
    E/OMXCodec(  151): dequeueBuffer failed: Invalid argument (22)
    E/OMXCodec(  151): [OMX.SEC.AVC.Decoder] Allocate Buffer failed - error = -22
    E/AwesomePlayer(  151): failed to start video source
    E/MediaPlayer( 2023): error (1, -22)
    E/MediaPlayer( 2023): Error (1,-22)
    D/VideoView( 2023): Error: 1,-22
    
    Change-Id: I9977dcf9fc50936038808ed06bcd85b07efd7bd8

--- media/libstagefright/OMXCodec.cpp
+++ media/libstagefright/OMXCodec.cpp
@@ -794,7 +794,7 @@ status_t OMXCodec::configureCodec(const sp<MetaData> &meta) {
             setVideoInputFormat(mMIME, meta);
 #endif
         } else {
-
+#ifdef QCOM_HARDWARE
             if (mNativeWindow != NULL
                 && !strncmp(mComponentName, "OMX.", 4)) {
                 status_t err = initNativeWindow();
@@ -803,7 +803,6 @@ status_t OMXCodec::configureCodec(const sp<MetaData> &meta) {
                 }
             }
 
-#ifdef QCOM_HARDWARE
             ExtendedCodec::configureVideoDecoder(
                     meta, mMIME, mOMX, mFlags, mNode, mComponentName);
 #endif
@@ -871,6 +870,18 @@ status_t OMXCodec::configureCodec(const sp<MetaData> &meta) {
 
         mQuirks &= ~kOutputBuffersAreUnreadable;
     }
+
+#ifndef QCOM_HARDWARE
+    if (mNativeWindow != NULL
+        && !mIsEncoder
+        && !strncasecmp(mMIME, "video/", 6)
+        && !strncmp(mComponentName, "OMX.", 4)) {
+        status_t err = initNativeWindow();
+        if (err != OK) {
+            return err;
+        }
+    }
+#endif
 
     return OK;
 }
