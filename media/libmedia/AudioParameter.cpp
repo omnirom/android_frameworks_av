5c800f20dc76aee1b383ecc5f23eccffd33cccd0
maxwen <max.weninger@gmail.com>
2014-12-05 02:11:03 +0100

    AudioParameter: sync with legacy CAF
    Change-Id: I0fff3a12b36c49a1a32701bb49f572c72c0fa525

--- media/libmedia/AudioParameter.cpp
+++ media/libmedia/AudioParameter.cpp
@@ -32,6 +32,8 @@ const char * const AudioParameter::keyChannels = AUDIO_PARAMETER_STREAM_CHANNELS
 const char * const AudioParameter::keyFrameCount = AUDIO_PARAMETER_STREAM_FRAME_COUNT;
 const char * const AudioParameter::keyInputSource = AUDIO_PARAMETER_STREAM_INPUT_SOURCE;
 const char * const AudioParameter::keyScreenState = AUDIO_PARAMETER_KEY_SCREEN_STATE;
+const char * const AudioParameter::keyFluenceType = AUDIO_PARAMETER_KEY_FLUENCE_TYPE;
+const char * const AudioParameter::keyADSPStatus = AUDIO_PARAMETER_KEY_ADSP_STATUS;
 
 AudioParameter::AudioParameter(const String8& keyValuePairs)
 {
