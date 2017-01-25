50e54012861772143c2f5c5d0c540988da99b22d
Karthikeyan Periasamy <kperiasa@codeaurora.org>
2017-01-25 14:53:12 -0800

    libstagefright: Check trackMeta for NULL
    getTrackMetaData will return NULL for failure. If we pass the NULL,
    we observe undefined behavior of mediaserver application .
    
    Check trackMeta and go to next index if it is NULL
    
    CRs-Fixed: 1115425
    Change-Id: I55a73cb0f327b6af31d30ed1b3d27c83578762d3

--- media/libstagefright/StagefrightMetadataRetriever.cpp
+++ media/libstagefright/StagefrightMetadataRetriever.cpp
@@ -256,6 +256,10 @@ status_t StagefrightMetadataRetriever::getFrameInternal(
     for (i = 0; i < n; ++i) {
         sp<MetaData> meta = mExtractor->getTrackMetaData(i);
 
+        if (meta == NULL) {
+            continue;
+        }
+
         const char *mime;
         CHECK(meta->findCString(kKeyMIMEType, &mime));
 
@@ -482,6 +486,10 @@ void StagefrightMetadataRetriever::parseMetaData() {
     for (size_t i = 0; i < numTracks; ++i) {
         sp<MetaData> trackMeta = mExtractor->getTrackMetaData(i);
 
+        if (trackMeta == NULL) {
+            continue;
+        }
+
         int64_t durationUs;
         if (trackMeta->findInt64(kKeyDuration, &durationUs)) {
             if (durationUs > maxDurationUs) {
@@ -604,7 +612,8 @@ void StagefrightMetadataRetriever::parseMetaData() {
                 !strcasecmp(fileMIME, "video/x-matroska")) {
             sp<MetaData> trackMeta = mExtractor->getTrackMetaData(0);
             const char *trackMIME;
-            CHECK(trackMeta->findCString(kKeyMIMEType, &trackMIME));
+            CHECK(trackMeta != NULL
+                  && trackMeta->findCString(kKeyMIMEType, &trackMIME));
 
             if (!strncasecmp("audio/", trackMIME, 6)) {
                 // The matroska file only contains a single audio track,
