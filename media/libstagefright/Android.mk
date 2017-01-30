2971b56ede7bd4b29dff4313fdbc8d612413b311
zakooz <marcotribosklol@gmail.com>
2017-01-30 00:25:38 +0000

    ACodec: Fix video autoscaling on old OMX decoders
    To enable: TARGET_OMX_LEGACY_RESCALING := true
    
    On Android N video autoscaling will cause the video to zoom in, being mostly outside of the frame of the video.
    This is because android no longer tries to match output ports before putting the new resolution in effect.
    Exynos OMX decoders send a message to get the new output crop, but it's lost along the way.
    Similarly to how Android M handles it, send the format change right before transitioning to ExecutingState.
    
    Change-Id: I19f974d37f9b11161efc7ee470301f444691fde6

--- media/libstagefright/Android.mk
+++ media/libstagefright/Android.mk
@@ -145,6 +145,10 @@ ifeq ($(TARGET_HAS_LEGACY_CAMERA_HAL1),true)
 LOCAL_CFLAGS += -DCAMCORDER_GRALLOC_SOURCE
 endif
 
+ifeq ($(TARGET_OMX_LEGACY_RESCALING),true)
+LOCAL_CFLAGS += -DUSE_LEGACY_RESCALING
+endif
+
 LOCAL_CFLAGS += -Wno-multichar -Werror -Wno-error=deprecated-declarations -Wall
 
 # enable experiments only in userdebug and eng builds
