4a9f2215bcea7f8c383218efb3c465a0f94609ee
pavanc <pavanc@codeaurora.org>
2017-06-27 20:33:46 +0530

    StagefrightRecorder: Reset AudioSourceNode during LPCM recording
    AudioSource is created as part of ExtendedAudioSource for LPCM
    recording. AudioSourceNode is not set to the right AudioSource
    in this case. Amplitude is not shown in SoundRecorder application
    due to this reason
    
    Fix is to reset AudioSourceNode to the instance that is returned
    by setPCMRecording
    
    CRs-Fixed: 2068175
    
    Change-Id: I13e6a78b8bdb1e5a23c398508100617e6fc8201a

--- media/libmediaplayerservice/StagefrightRecorder.cpp
+++ media/libmediaplayerservice/StagefrightRecorder.cpp
@@ -1126,10 +1126,11 @@ sp<MediaCodecSource> StagefrightRecorder::createAudioSource() {
 
     sp<MediaCodecSource> audioEncoder =
             MediaCodecSource::Create(mLooper, format, audioSource);
-    mAudioSourceNode = audioSource;
 
     if (audioEncoder == NULL) {
         ALOGE("Failed to create audio encoder");
+    } else {
+        mAudioSourceNode = audioSource;
     }
 
     return audioEncoder;
@@ -1195,6 +1196,7 @@ status_t StagefrightRecorder::setupRawAudioRecording() {
             ALOGE("Recording source is null");
             return BAD_VALUE;
         }
+        mAudioSourceNode =  reinterpret_cast<AudioSource* > (src.get());
         CHECK(mWriter != 0);
         mWriter->addSource(src);
     }
