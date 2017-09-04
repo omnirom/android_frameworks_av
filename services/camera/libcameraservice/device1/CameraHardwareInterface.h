4cb4015d9ac9be43edd6fa696951b96e71a01b98
LuK1337 <priv.luk@gmail.com>
2017-09-05 01:15:18 +0200

    Revert "Camera: Cleanup 'CameraModule' dependend code"
    This reverts commit f53f66edb3b06d1df5caf1fa806f7ed95305a4cf.
    
    Change-Id: Id34f4a2689c70ba25df48cf0d65fcea24d44344e

--- services/camera/libcameraservice/device1/CameraHardwareInterface.h
+++ services/camera/libcameraservice/device1/CameraHardwareInterface.h
@@ -28,6 +28,7 @@
 #include <system/window.h>
 #include <hardware/camera.h>
 
+#include <common/CameraModule.h>
 #include <common/CameraProviderManager.h>
 #include <vendor/qti/hardware/camera/device/1.0/IQCameraDeviceCallback.h>
 
@@ -107,6 +108,7 @@ public:
 
     ~CameraHardwareInterface();
 
+    status_t initialize(CameraModule *module);
     status_t initialize(sp<CameraProviderManager> manager);
 
     /** Set the ANativeWindow to which preview frames are sent */
