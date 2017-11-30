3aa710dac858ac87a637ed5fdf4f980ea32d29c9
Naresh Tanniru <ntanniru@codeaurora.org>
2017-11-30 19:10:03 +0530

    audio: Create dummy track to avoid a2dp suspend
    - With split a2dp, suspend command to remote BT
      device is observed while performing next/prev
      operations from remote BT devices.
    - During Next/prev operations since audio frameworks
      gets stop called before starting next track, no active
      tracks are available from HAL/DSP.This resulting
      a2dp stop being called from audio HAL to IPC
      library.
    - Create dummy low latency session for offload usecase
      during session closer.
    
    CRs-Fixed: 2272032
    Change-Id: Idcea4863a6a578e43839aca4947b9f26ff9484d0

--- media/libaudioclient/include/media/AudioTrack.h
+++ media/libaudioclient/include/media/AudioTrack.h
@@ -998,6 +998,8 @@ protected:
 
             void     updateRoutedDeviceId_l();
 
+            void     createDummyAudioSessionForA2DP();
+
     // Next 4 fields may be changed if IAudioTrack is re-created, but always != 0
     sp<IAudioTrack>         mAudioTrack;
     sp<IMemory>             mCblkMemory;
@@ -1173,6 +1175,8 @@ protected:
                                               // activity and connected devices.
 
     sp<media::VolumeHandler>       mVolumeHandler;
+
+    int64_t                mPauseTimeRealUs;
 
 private:
     class DeathNotifier : public IBinder::DeathRecipient {
