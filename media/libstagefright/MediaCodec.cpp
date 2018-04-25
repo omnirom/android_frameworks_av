44af053144aadec55aa966cb018cb6e6946d634c
Preetam Singh Ranawat <apranawat@codeaurora.org>
2018-04-25 13:47:01 +0530

    Audio: Add gracefull exit for extended audio codecs.
     -Extended Audio codecs are not present in media codec list
      so gracefully return error if media codec info is NULL.
     -include extended video and audio flac codec for codec info
      check using media codec list as they are present in list.
    
    CRs-Fixed: 2231627
    Change-Id: If0ccbe1e129c3662c8f98d9c41f8f2566b9767ff

--- media/libstagefright/MediaCodec.cpp
+++ media/libstagefright/MediaCodec.cpp
@@ -896,8 +896,9 @@ status_t MediaCodec::init(const AString &name, bool nameIsType) {
     //make sure if the component name contains qcom/qti, we don't return error
     //as these components are not present in media_codecs.xml and MediaCodecList won't find
     //these component by findCodecByName
-    if (!(name.find("qcom", 0) > 0 ||
-        name.find("qti", 0) > 0) || name.find("video", 0) > 0) {
+    //Video and Flac decoder are present in list so exclude them.
+    if (!(name.find("qcom", 0) > 0 || name.find("qti", 0) > 0)
+          || name.find("video", 0) > 0 || name.find("flac", 0) > 0) {
         const sp<IMediaCodecList> mcl = MediaCodecList::getInstance();
         if (mcl == NULL) {
             mCodec = NULL;  // remove the codec.
@@ -1520,6 +1521,11 @@ status_t MediaCodec::getCodecInfo(sp<MediaCodecInfo> *codecInfo) const {
 
     sp<RefBase> obj;
     CHECK(response->findObject("codecInfo", &obj));
+
+    if (static_cast<MediaCodecInfo *>(obj.get()) == nullptr) {
+        ALOGE("codec info not found");
+        return NAME_NOT_FOUND;
+    }
     *codecInfo = static_cast<MediaCodecInfo *>(obj.get());
 
     return OK;
