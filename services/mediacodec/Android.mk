8e926bb0668d2b62e7618d0d5f3f96f9ac73f406
Paras Nagda <pnagda@codeaurora.org>
2018-07-26 13:27:08 +0530

    Tuning of Binder buffer for various HALs
    Tuning of Binder buffer is required for 32 bit targets.
    
    CRs-Fixed: 2287417
    
    Change-Id: Ibc6ad0150894a1578ac89f9412b9b8027f5d8d71

--- services/mediacodec/Android.mk
+++ services/mediacodec/Android.mk
@@ -51,6 +51,11 @@ LOCAL_MODULE := android.hardware.media.omx@1.0-service
 LOCAL_MODULE_RELATIVE_PATH := hw
 LOCAL_VENDOR_MODULE := true
 LOCAL_32_BIT_ONLY := true
+
+ifeq ($(TARGET_ARCH),arm)
+     LOCAL_CFLAGS += -DENABLE_BINDER_BUFFER_TUNING_FOR_32_BIT
+endif
+
 # Since this is 32-bit-only module, only 32-bit version of the codecs are installed.
 # TODO(b/72343507): eliminate the need for manually adding .vendor suffix. This should be done
 # by the build system.
