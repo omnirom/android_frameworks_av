cadd5b7f37cbc05d92bd1f8f9a47a2b480af84cb
Marko Man <darkobas@gmail.com>
2018-08-25 10:42:04 +0200

    add qcom guard
    Change-Id: I51b46ff7d2b1edfe9a3d225743f8d38d411f71b7

--- camera/cameraserver/Android.mk
+++ camera/cameraserver/Android.mk
@@ -30,9 +30,13 @@ LOCAL_SHARED_LIBRARIES := \
 	android.hardware.camera.common@1.0 \
 	android.hardware.camera.provider@2.4 \
 	android.hardware.camera.device@1.0 \
-	vendor.qti.hardware.camera.device@1.0 \
 	android.hardware.camera.device@3.2 \
 	android.hidl.manager@1.0
+
+ifeq ($(BOARD_USES_QCOM_HARDWARE),true)
+LOCAL_SHARED_LIBRARIES += \
+    vendor.qti.hardware.camera.device@1.0
+endif
 
 LOCAL_MODULE:= cameraserver
 LOCAL_32_BIT_ONLY := true
--- services/camera/libcameraservice/Android.mk
+++ services/camera/libcameraservice/Android.mk
@@ -79,10 +79,14 @@ LOCAL_SHARED_LIBRARIES:= \
     android.hardware.camera.common@1.0 \
     android.hardware.camera.provider@2.4 \
     android.hardware.camera.device@1.0 \
-    vendor.qti.hardware.camera.device@1.0 \
     android.hardware.camera.device@3.2 \
     android.hardware.camera.device@3.3 \
     android.hardware.camera.device@3.4
+
+ifeq ($(BOARD_USES_QCOM_HARDWARE),true)
+LOCAL_SHARED_LIBRARIES += \
+    vendor.qti.hardware.camera.device@1.0
+endif
 
 LOCAL_EXPORT_SHARED_LIBRARY_HEADERS := libbinder libcamera_client libfmq
 
