Jackeagle/caf_frameworks_av

media: Changes to pick target specific media xml
Add changes to pick the right media xml's from
vendor/etc of the target based on target specific
system property for SDM660 and SDM455.

Change-Id: Iece1e95ab111931ac937acb8799b575fd01294a0
CRs-Fixed: 2288926

media/libstagefright/xmlparser/MediaCodecsXmlParser.cpp
@@ -172,6 +172,17 @@ MediaCodecsXmlParser::MediaCodecsXmlParser(
                 ALOGE("SDM429 prop_value = %s, file_path = %s", value, file_path);
                 path = file_path;
                 parseTopLevelXMLFile(path.c_str(), false);
+            } else if (!strcmp(platform, "sdm660")) {
+                if (property_get("vendor.media.sdm660.version", value, "0") &&
+                    (atoi(value) == 1)){
+                    strlcpy(file_path, "/vendor/etc/media_codecs_sdm660_v1.xml",
+                            PROP_VALUE_MAX);
+                } else {
+                    strlcpy(file_path, "/vendor/etc/media_codecs.xml",
+                            PROP_VALUE_MAX);
+                }
+                path = file_path;
+                parseTopLevelXMLFile(path.c_str(), false);
             } else {
                 parseTopLevelXMLFile(path.c_str(), false);
             }
@@ -200,6 +211,15 @@ MediaCodecsXmlParser::MediaCodecsXmlParser(
                             PROP_VALUE_MAX);
                 } else {
                     strlcpy(file_path, "/vendor/etc/media_codecs_performance_8953.xml",
+                            PROP_VALUE_MAX);
+                }
+            } else if (!strcmp(platform, "sdm660")) {
+                if (property_get("vendor.media.sdm660.version", value, "0") &&
+                    (atoi(value) == 1)){
+                    strlcpy(file_path, "/vendor/etc/media_codecs_performance_sdm660_v1.xml",
+                            PROP_VALUE_MAX);
+                } else {
+                    strlcpy(file_path, "/vendor/etc/media_codecs_performance.xml",
                             PROP_VALUE_MAX);
                 }
             } else {

omnirom/android_frameworks_av

media/libstagefright/xmlparser/MediaCodecsXmlParser.cpp
@@ -172,6 +172,17 @@ MediaCodecsXmlParser::MediaCodecsXmlParser(
                 ALOGE("SDM429 prop_value = %s, file_path = %s", value, file_path);
                 path = file_path;
                 parseTopLevelXMLFile(path.c_str(), false);
+            } else if (!strcmp(platform, "sdm660")) {
+                if (property_get("vendor.media.sdm660.version", value, "0") &&
+                    (atoi(value) == 1)){
+                    strlcpy(file_path, "/vendor/etc/media_codecs_sdm660_v1.xml",
+                            PROP_VALUE_MAX);
+                } else {
+                    strlcpy(file_path, "/vendor/etc/media_codecs_vendor.xml",
+                            PROP_VALUE_MAX);
+                }
+                path = file_path;
+                parseTopLevelXMLFile(path.c_str(), false);
             } else {
                 parseTopLevelXMLFile(path.c_str(), false);
             }
@@ -200,6 +211,15 @@ MediaCodecsXmlParser::MediaCodecsXmlParser(
                             PROP_VALUE_MAX);
                 } else {
                     strlcpy(file_path, "/vendor/etc/media_codecs_performance_8953.xml",
+                            PROP_VALUE_MAX);
+                }
+            } else if (!strcmp(platform, "sdm660")) {
+                if (property_get("vendor.media.sdm660.version", value, "0") &&
+                    (atoi(value) == 1)){
+                    strlcpy(file_path, "/vendor/etc/media_codecs_performance_sdm660_v1.xml",
+                            PROP_VALUE_MAX);
+                } else {
+                    strlcpy(file_path, "/vendor/etc/media_codecs_performance.xml",
                             PROP_VALUE_MAX);
                 }
             } else {