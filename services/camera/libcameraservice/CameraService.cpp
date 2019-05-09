9457842f6885d43739c841ba3fece9a6619765b0
Marko Man <darkobas@gmail.com>
2019-05-09 13:47:13 +0200

    libcameraservice: write all pkgNames
    Change-Id: I85c2e9d4ffb56cc08aea1c1eabc1ed2ec45c0217

--- services/camera/libcameraservice/CameraService.cpp
+++ services/camera/libcameraservice/CameraService.cpp
@@ -2270,11 +2270,7 @@ status_t CameraService::BasicClient::startCameraOps() {
 #ifdef TARGET_NEEDS_CLIENT_INFO
     std::ofstream cpf("/data/vendor/omni/client_package_name");
     std::string cpn = String8(mClientPackageName).string();
-    if (cpn.compare("com.oneplus.camera") == 0) {
-        cpf << "com.oneplus.camera";
-    } else {
-        cpf << "0";
-    }
+    cpf << cpn;
 #endif
     return OK;
 }
