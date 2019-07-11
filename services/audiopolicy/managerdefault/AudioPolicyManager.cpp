47eb7edc4912f9bbdb60d4ffd5363f9185d9ddc9
Manisha Agarwal <maniagar@codeaurora.org>
2019-07-11 17:15:21 +0530

    APM: Wrong device selection for music during concurrency.
    Music playback is switching from WFD to speaker when ringtone is
    played and speaker to hanset when ringtone is played.
    
    The issue is wrong device is getting returned from getNewOutputDevices
    which selects device on basis of active product strategy so wrong device is
    selected based on the order of product strategies.
    
    To avoid this we add a check to see if the Alarm stream or Enforced Audible
    stream is actually active,if current output desc has VOICE_CALL stream.
    
    CRs-Fixed: 2446622
    Change-Id: Iff6aa67f799cec5f960707fb39333393944d1478

--- services/audiopolicy/managerdefault/AudioPolicyManager.cpp
+++ services/audiopolicy/managerdefault/AudioPolicyManager.cpp
@@ -5231,9 +5231,10 @@ DeviceVector AudioPolicyManager::getNewOutputDevices(const sp<SwAudioOutputDescr
         StreamTypeVector streams = mEngine->getStreamTypesForProductStrategy(productStrategy);
         auto attr = mEngine->getAllAttributesForProductStrategy(productStrategy).front();
 
-        if ((hasVoiceStream(streams) &&
+        if (((hasVoiceStream(streams) && outputDesc->isActive(toVolumeSource(AUDIO_STREAM_VOICE_CALL))) &&
              (isInCall() || mOutputs.isStrategyActiveOnSameModule(productStrategy, outputDesc))) ||
-             ((hasStream(streams, AUDIO_STREAM_ALARM) || hasStream(streams, AUDIO_STREAM_ENFORCED_AUDIBLE)) &&
+             (((hasStream(streams, AUDIO_STREAM_ALARM) && isStreamActive(AUDIO_STREAM_ALARM, 0)) ||
+               (hasStream(streams, AUDIO_STREAM_ENFORCED_AUDIBLE) && isStreamActive(AUDIO_STREAM_ENFORCED_AUDIBLE, 0))) &&
                 mOutputs.isStrategyActiveOnSameModule(productStrategy, outputDesc)) ||
                 outputDesc->isStrategyActive(productStrategy)) {
             // Retrieval of devices for voice DL is done on primary output profile, cannot
