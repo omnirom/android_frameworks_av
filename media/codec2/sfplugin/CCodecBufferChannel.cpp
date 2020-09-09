bf6316ac88a1c01b35d3086f0f90cc4581d97e0b
Brijesh Patel <pbrijesh@codeaurora.org>
2020-09-09 17:30:01 +0530

    CCodec: Do not print component name in debug logs if uninitialized
    - It may happen that watchdog timeout before component gets created
      and set, in which case component name might not be initialized
      before print debug log.
    
    CRs-Fixed: 2773741
    
    Change-Id: I80db0be6a9761dc0fc4a1b3fcde42ccecccf8180

--- media/codec2/sfplugin/CCodecBufferChannel.cpp
+++ media/codec2/sfplugin/CCodecBufferChannel.cpp
@@ -1865,8 +1865,9 @@ PipelineWatcher::Clock::duration CCodecBufferChannel::elapsed() {
         size_t outputDelay = mOutput.lock()->outputDelay;
         Mutexed<Input>::Locked input(mInput);
         n = input->inputDelay + input->pipelineDelay + outputDelay + kSmoothnessFactor;
-        ALOGD("[%s] DEBUG: elapsed: n=%zu [in=%u pipeline=%u out=%zu smoothness=%zu]", mName, n,
-                input->inputDelay, input->pipelineDelay, outputDelay, kSmoothnessFactor);
+        ALOGD("[%s] DEBUG: elapsed: n=%zu [in=%u pipeline=%u out=%zu smoothness=%zu]",
+              mComponentName.c_str(), n, input->inputDelay, input->pipelineDelay,
+              outputDelay, kSmoothnessFactor);
     }
     return mPipelineWatcher.lock()->elapsed(PipelineWatcher::Clock::now(), n);
 }
