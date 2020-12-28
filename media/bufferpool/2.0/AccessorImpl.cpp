106198a058326278f7b6a611700901e9a93ab9f1
maxwen <max.weninger@gmail.com>
2020-12-28 16:55:31 +0100

    av: disable somne logspilling during playback
    Change-Id: I76d0a37a1b7ae74450f5fa93c6139e97040e4126

--- media/bufferpool/2.0/AccessorImpl.cpp
+++ media/bufferpool/2.0/AccessorImpl.cpp
@@ -320,7 +320,7 @@ std::atomic<std::uint32_t> Accessor::Impl::BufferPool::Invalidation::sInvSeqId(0
 
 Accessor::Impl::Impl::BufferPool::~BufferPool() {
     std::lock_guard<std::mutex> lock(mMutex);
-    ALOGD("Destruction - bufferpool2 %p "
+    ALOGV("Destruction - bufferpool2 %p "
           "cached: %zu/%zuM, %zu/%d%% in use; "
           "allocs: %zu, %d%% recycled; "
           "transfers: %zu, %d%% unfetched",
@@ -496,7 +496,7 @@ bool Accessor::Impl::BufferPool::handleTransferTo(const BufferStatusMessage &mes
     }
     if (mConnectionIds.find(message.targetConnectionId) == mConnectionIds.end()) {
         // N.B: it could be fake or receive connection already closed.
-        ALOGD("bufferpool2 %p receiver connection %lld is no longer valid",
+        ALOGV("bufferpool2 %p receiver connection %lld is no longer valid",
               this, (long long)message.targetConnectionId);
         return false;
     }
@@ -728,7 +728,7 @@ void Accessor::Impl::BufferPool::cleanUp(bool clearCache) {
         mLastCleanUpUs = mTimestampUs;
         if (mTimestampUs > mLastLogUs + kLogDurationUs) {
             mLastLogUs = mTimestampUs;
-            ALOGD("bufferpool2 %p : %zu(%zu size) total buffers - "
+            ALOGV("bufferpool2 %p : %zu(%zu size) total buffers - "
                   "%zu(%zu size) used buffers - %zu/%zu (recycle/alloc) - "
                   "%zu/%zu (fetch/transfer)",
                   this, mStats.mBuffersCached, mStats.mSizeCached,
