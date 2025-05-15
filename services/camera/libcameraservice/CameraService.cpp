ddffcb5ab8f2fa6b9e3f0b0962ae9558a6652101
micky387 <mickaelsaibi@free.fr>
2025-05-14 22:13:18 -0400

    Fixup! libcameraservice: add TARGET_CAMERA_NEEDS_CLIENT_INFO
    after https://android.googlesource.com/platform/frameworks/av/+/2e772b80616a879e12f61ecd36e7afe9249a10cd%5E%21/#F0
    
    Change-Id: I00342ee585feb1a320c2f90d7f29224e172adaf2
    Signed-off-by: micky387 <mickaelsaibi@free.fr>

--- services/camera/libcameraservice/CameraService.cpp
+++ services/camera/libcameraservice/CameraService.cpp
@@ -4441,7 +4441,7 @@ status_t CameraService::BasicClient::notifyCameraOpening() {
 
 #ifdef TARGET_CAMERA_NEEDS_CLIENT_INFO
     std::ofstream cpf("/data/misc/omni/client_package_name");
-    std::string cpn = mClientPackageName.c_str();
+    std::string cpn = getPackageName().c_str();
     cpf << cpn;
 #endif
 
